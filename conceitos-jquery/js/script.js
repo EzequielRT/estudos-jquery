ab540d8305653b7ede1a90317ff59e072b3c2baf
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 00:43:21 -0300

    Selecionando elemento, adc classe e alterando HTML

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,12 +1,8 @@
 $(()=>{
 
-    // Setando atritubutos
-    $('a').attr('href', 'http://google.com.br'); 
-    $('img').attr('width', '200');
-    $('img').attr('border', '5');
-
-    // Retorna o valor que esta setado no elemento
-    console.log($('img').attr('border')); // = 5
+    $('#teste').find('button').addClass('estilo'); // Adicionado classe ao botao
+    $('#teste').find('button').html('Novo nome do meu botão'); // Altera HTML do botao
 
 });
 
+// $('#teste').html("Altera HTML") -> Altera HTML da pagina
