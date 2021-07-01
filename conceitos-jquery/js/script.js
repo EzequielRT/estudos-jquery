f40cbaf392fcb2da42dc652e8b525bc389901f85
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 01:15:02 -0300

    Manipulando elementos

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,8 +1,11 @@
 $(()=>{
 
-    $('#teste').find('button').addClass('estilo'); // Adicionado classe ao botao
-    $('#teste').find('button').html('Novo nome do meu botão'); // Altera HTML do botao
+    //$('#teste').text('Agora isto é um texto'); // Altera elemento HTML e adc texto
+    $('#teste').find('img').width(200);
+    $('input').val('Teste'); // Altera o value do elemento
+    $('input').before('<div>Adiciona conteudo antes do elemento</div>');
+    $('input').after('<br><img src="http://www.google.com.br/google.jpg">'); // Adiciona conteudo apos o elemento    
+    $('ul').prepend("<li>Item 0</li>"); // Adiciona conteudo ao HTML sem apagar o anterior - antes
+    $('ul').append("<li>Item 5</li>"); // Adiciona conteudo ao HTML sem apagar o anterior - depois
 
 });
-
-// $('#teste').html("Altera HTML") -> Altera HTML da pagina
