07a3ebffb1af9b4aa19b358e8f8f9a631dc0855e
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-06-30 23:25:24 -0300

    Selecionando Elementos do HTML

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,4 +1,4 @@
-/*var $j = jQuery.noConflict();
-$j(document).ready(()=> (alert("Está funcionando...")));*/
-
-jQuery(document).ready(()=> (alert("Funcionando nativamente...")));
+// $('.botao') -> Seleciona pela classe do elemento
+// $('#botao1') -> Seleciona pelo id do elemento,
+// $('li.botao') -> Seleciona a tag li que tiver a classe botao
+// $('.lista2 li.botao') -> Seleciona a tag li que tiver dentro da classe .lista2 e tiver a classe botao
