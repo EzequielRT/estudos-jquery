1672512fc27e26716f64aee846c414b49e42c02e
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 22:54:47 -0300

    Itera elementos HTML com each()

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,4 +1,10 @@
-    
+    /*
     var valor = $('#algo').html();
     valor = $.trim(valor);  // Remove os espaços do elemento
-    
+    */
+
+    var valor = $.trim($('#algo').html()); // Faz a msm coisa
+
+    $('li').each(function () { // Itera elementos HTML 
+        alert($(this).html());
+    })
