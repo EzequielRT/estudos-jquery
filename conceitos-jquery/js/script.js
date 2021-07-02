69c188bbd8523c03e426324af7b0e095168ae4bd
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 23:31:29 -0300

    Pegando item selecionado de SELECT

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,5 +1,1 @@
-$('#nome').attr('disabled', 'disabled');
-$('#nome').removeAttr('disabled', 'disabled');
-
-$('#salvo').attr('checked', 'checked');
-$('#salvo').removeAttr('checked', 'checked');
+$('#idade').val();
