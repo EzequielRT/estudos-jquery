5f3138df2b1e2a9199f34315b9643ced4fbbe961
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 21:53:23 -0300

    Adicionando e removendo classes

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,15 +1,4 @@
 $(()=>{
-
-    /* Formas de selecionar a class .filha
-    $('.filha');
-    $('.site').find('.filha');
-    $('.site').find('.topo').find('.filha'); */
-
-    //$('.filha').parent() // Seleciona o elemento HTML que esta antes da class .filha = .subtopo
-
-    //$('.irmao').closest('.topo'); // Acha o elemento que estiver mais perto e que tenha a classe .topo
-
-     //$('.irmao').closest('.site').find('.conteudo'); // Retorna para a div que engloba todo o site para selecionar a div que estiver fora do contexto da primeira selecionada
-
-     // OBS: Preferir usar closest
+    $('h1').addClass('fundovermelho'); // Adiciona class
+    $('h1').removeClass('fundovermelho'); // Remove class
 });
