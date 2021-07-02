35f27d10f93a18d2edda80a9f2ae358f8e10157e
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 23:05:35 -0300

    Retorna TIPO do dado

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,10 +1,3 @@
-    /*
-    var valor = $('#algo').html();
-    valor = $.trim(valor);  // Remove os espaços do elemento
-    */
+var nome = "Ezequiel";
 
-    var valor = $.trim($('#algo').html()); // Faz a msm coisa
-
-    $('li').each(function () { // Itera elementos HTML 
-        alert($(this).html());
-    })
+$.type(nome); // Retorna TIPO do dado
