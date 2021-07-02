a19dc06d2d2d9d8c2d065b71660cbf6f52ca2748
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 22:53:51 -0300

    Remove espacos de elementos HTML

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,8 +1,4 @@
-$(()=>{
-
-    $('input').attr('data-idade', 90); // Armazena dado no elemento HTML
-    $('input').data('idade', 85) // Armazena dado na memoria do jQuery
     
-    $('input').data('caracteres', $('input').val().length); // Salva a qtd de caracteres que tem na string
-
-});
+    var valor = $('#algo').html();
+    valor = $.trim(valor);  // Remove os espaços do elemento
+    
