8afb2a568e1df233fe678fc7c250d4c34e1b7e5b
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 23:12:15 -0300

    Retornando os tipos da variaveis

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,3 +1,5 @@
-var nome = "Ezequiel";
+var num = 5;
 
-$.type(nome); // Retorna TIPO do dado
+$.isNumeric(num); // Retorna se a variavel é um numero = true/false
+$.isArray(num); // Retorna se a variavel é um array = true/false
+$.isFunction(num); // Retorna se a variavel é uma funcao = true/false
