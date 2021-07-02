9363bdc01fe6617a7a3f94d27f1f991982a4b0a4
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-01 21:56:28 -0300

    Verifica se a tag possui determinada classe

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,4 +1,10 @@
 $(()=>{
     $('h1').addClass('fundovermelho'); // Adiciona class
-    $('h1').removeClass('fundovermelho'); // Remove class
+    //$('h1').removeClass('fundovermelho'); // Remove class
+
+    if( $('h1').hasClass('fundovermelho') ) { // Verifica se a tag possui a classe, retorna true ou false
+        alert("Tem sim");
+    } else { 
+        alert("Tem não");
+    }
 });
