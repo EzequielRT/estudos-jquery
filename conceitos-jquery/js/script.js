b7b7354174639afb98178e9352e238ebd1cfa58a
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-02 21:48:49 -0300

    Maneiras de adc evento de clique

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,7 +1,15 @@
 $(function() {
     
-    $('button').hover(
-        function() { $(this).toggleClass('fundovermelho') }
+    // Adiciona evento de clique
+    $('button').bind('click',
+        function() { $(this).toggleClass('fundovermelho') } 
     );
+
+    /*
+    // Adiciona evento de clique
+    $('button').on('click',
+        function() { $(this).toggleClass('fundovermelho') } 
+    );
+    */
 
 });
