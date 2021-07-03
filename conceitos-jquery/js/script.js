9b34c10f4b57e93c2d0f2f1dd4ca9fb11e6b78c4
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-02 21:36:57 -0300

    Funcao Hover para adc e remover classes

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,19 +1,8 @@
 $(function() {
-    $('button').click(function(){
-        /*if($(this).hasClass('fundovermelho')) {
-            $(this).removeClass('fundovermelho');
-        } else {
-            $(this).addClass('fundovermelho');
-        };*/
+    
+    $('button').hover(
+        function() { $(this).addClass('fundovermelho') }, 
+        function() { $(this).removeClass('fundovermelho') }
+    );
 
-        $(this).toggleClass('fundovermelho'); // Faz a mesma logica que a realizada acima
-    });
-
-    $('button').mouseover(function() {
-        $(this).css('font-weight', 'bold');
-    });
-
-    $('button').mouseout(function() {
-        $(this).css('font-weight', 'normal');
-    });
 });
