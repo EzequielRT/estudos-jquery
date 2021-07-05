51124efb4a7291c3c129bd6cf5899b24a7495a0a
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-04 23:36:54 -0300

    Eventos de Mouse - mouseover e mouseout

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,11 +1,11 @@
 $(function () {
     
-    $('.botao').bind('mousemove', function (){
-        console.log("Moveu o mouse.")
+    $('.botao').bind('mouseover', function (){
+        $(this).addClass('botao_emcima')
     });
 
-    $('.botao').bind('click', function (){
-        console.log("Clicou.")
+    $('.botao').bind('mouseout', function (){
+        $(this).removeClass('botao_emcima')
     });
 
 });
