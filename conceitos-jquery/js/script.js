78a8fdb5797b7046e1f7ea4cf3a1e14abe11e5e0
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-04 23:52:02 -0300

    Efeito para o elemento aparecer-desaparecer

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,7 +1,14 @@
 $(function () {
     
-    $('.botao').bind('dblclick', function (){
-        console.log("Deu dois cliques em um elemento.");
-    });
+    $('.div').hide(); // Aplica css display none no elemento
+    $('.div').show(); // Faz com que o elemento apareca
+
+    $('.div').hide('slow'); // Aplica css display none no elemento com parametro devagar
+    $('.div').show('slow'); // Faz com que o elemento apareca com parametro devagar
+
+
+    // Se o elemento esta visivel ele aplica o contrario e vice-versa
+    $('.div').toggle('fast'); 
+    $('.div').toggle('fast');
 
 });
