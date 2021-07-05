43d44a2824c9c07dcfcfc8b257a777d927140e12
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-04 23:57:06 -0300

    Efeito para o elemento aparecer-desaparecer com fadeIn/Out

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,14 +1,8 @@
 $(function () {
     
-    $('.div').hide(); // Aplica css display none no elemento
-    $('.div').show(); // Faz com que o elemento apareca
-
-    $('.div').hide('slow'); // Aplica css display none no elemento com parametro devagar
-    $('.div').show('slow'); // Faz com que o elemento apareca com parametro devagar
-
-
-    // Se o elemento esta visivel ele aplica o contrario e vice-versa
-    $('.div').toggle('fast'); 
-    $('.div').toggle('fast');
+    $('.botao').bind('click', function(){
+        $('.div').fadeOut();
+        $('.div').fadeIn();
+    });
 
 });
