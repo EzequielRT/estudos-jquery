b229dc0c88bd2ea653ab2d0a78ac599f8e177aab
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-04 23:35:52 -0300

    Eventos de Mouse - mousemove e click

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,11 +1,11 @@
 $(function () {
     
-    $('.botao').bind('mousedown', function (){
-        console.log("Apertou o mouse.")
+    $('.botao').bind('mousemove', function (){
+        console.log("Moveu o mouse.")
     });
 
-    $('.botao').bind('mouseup', function (){
-        console.log("Soltou o mouse.")
+    $('.botao').bind('click', function (){
+        console.log("Clicou.")
     });
 
 });
