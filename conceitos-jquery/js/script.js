1701a50fc66fe0cf8a0ae89deb3d905bfa519c02
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-04 23:59:34 -0300

    Efeito para o elemento aparecer-desaparecer com fadeTo

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,8 +1,7 @@
 $(function () {
     
     $('.botao').bind('click', function(){
-        $('.div').fadeOut();
-        $('.div').fadeIn();
+        $('.div').fadeTo('slow', 0.5);  // Aplica o fadeOut com parametro de velocidade e nivel de 0 ate 1
     });
 
 });
