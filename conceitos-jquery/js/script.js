8d2f09c7a2de344257466368fc51d587e636764b
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-07 20:20:03 -0300

    Requisição Ajax Completa

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -5,6 +5,19 @@ $(function () {
         
         var txt = $(this).serialize(); // Formata os dados para envio
         console.log(txt);
+
+        // Funcao que faz requisicoes Ajax
+        $.ajax({  
+            type:'POST',
+            url:'ajax.php',
+            data:txt,  // Informacoes que estao sendo enviadas
+            success:function(resultado){
+                $('.div').html("Resultado: "+resultado);
+            },
+            error:function(){
+                alert("Ocorreu um erro!");
+            }
+        }); 
     });
 
 });
