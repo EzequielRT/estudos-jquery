ad7de29298c4b4a0bf75d1a8df17bfa2fd765b17
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-06 22:20:28 -0300

    Parando uma animacao em andamento com a funcao stop

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,23 +1,13 @@
 $(function () {
     
-    $('.botao').bind('click', function(){
+    $('#btn1').bind('click', function(){
         $('.div').animate({
-            'margin-left':'100px',
-            'margin-top':'20px',
-            'width':'500px',
-            'border-radius':'75px'
-        }, {
-            duration:1500,
-            start:function(){
-                console.log("Animação começou!");
-            },
-            complete:function(){
-                console.log("Animação finalizada!");
-            },
-            step:function(){
-                console.log("Nova etapa...");
-            }
-        });
+            'margin-left':'500px'
+        }, 2000);
+    });
+
+    $('#btn2').bind('click', function(){
+        $('.div').stop();
     });
 
 });
