f3848703ca844d080c8cc99766bfd32a92e53f5c
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-07 22:34:34 -0300

    Recebendo e manipulando JSON via Ajax

--- conceitos-jquery/ajax.php
+++ conceitos-jquery/ajax.php
@@ -1,4 +1,11 @@
 <?php
-$x = intval($_POST['x']);
-$y = intval($_POST['y']);
-echo $x+$y;
+$nome = $_POST['nome'];
+$senha = $_POST['senha'];
+
+$array = array('status'=>'');
+
+if($nome == 'Ezequiel' && $senha == '1234') {
+    $array['status'] = 'ok';
+}
+
+echo json_encode($array);
--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -4,18 +4,19 @@ $(function () {
         e.preventDefault();
         
         var txt = $(this).serialize(); // Formata os dados para envio
-        console.log(txt);
 
         // Funcao que faz requisicoes Ajax
         $.ajax({  
             type:'POST',
             url:'ajax.php',
             data:txt,  // Informacoes que estao sendo enviadas
-            success:function(resultado){
-                $('.div').html("Resultado: "+resultado);
-            },
-            error:function(){
-                alert("Ocorreu um erro!");
+            dataType:'json', // Seta que as informacoes recebidas serao no formato json
+            success:function(json){
+                if(json.status == 'ok') {
+                    alert("Deu certo!");
+                } else {
+                    alert("Deu errado!");
+                }
             }
         }); 
     });
