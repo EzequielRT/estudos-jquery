c7a886c699cffe2da79fcc863ba17edb149d6ca4
Ezequiel Rodrigues Trindade <ezefuts@gmail.com>
2021-07-07 22:55:58 -0300

    Como usar um Plugin com jQuery

--- conceitos-jquery/js/script.js
+++ conceitos-jquery/js/script.js
@@ -1,24 +1,5 @@
 $(function () {
     
-    $('#form').bind('submit', function(e){
-        e.preventDefault();
-        
-        var txt = $(this).serialize(); // Formata os dados para envio
-
-        // Funcao que faz requisicoes Ajax
-        $.ajax({  
-            type:'POST',
-            url:'ajax.php',
-            data:txt,  // Informacoes que estao sendo enviadas
-            dataType:'json', // Seta que as informacoes recebidas serao no formato json
-            success:function(json){
-                if(json.status == 'ok') {
-                    alert("Deu certo!");
-                } else {
-                    alert("Deu errado!");
-                }
-            }
-        }); 
-    });
+    $('#cep').mask('00000-000');
 
 });
